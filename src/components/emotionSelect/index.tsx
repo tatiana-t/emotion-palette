9efcbd59b9212aaef373cb6bb12ec5172237ebaa
Tatiana <grabli_77@mail.ru>
2025-08-15 15:36:13 +0300

    style emo select

--- src/components/emotionSelect/index.tsx
+++ src/components/emotionSelect/index.tsx
@@ -1,40 +1,88 @@
-import { Select } from '@gravity-ui/uikit';
+import { useState, useEffect } from 'react';
+import classnames from 'classnames';
 import { useDataStore } from 'src/storage';
+import list from 'src/data/emotions';
 import './styles.scss';
 
 interface Props {
   onAnswer: (isAnswered: boolean) => void;
 }
 
-const list = [
-  { value: '1', content: 'Радость' },
-  { value: '2', content: 'Печаль' },
-];
-
 const EmotionSelect: React.FC<Props> = ({ onAnswer }) => {
   const updateToday = useDataStore((state) => state.updateToday);
   const today = useDataStore((state) => state.today);
+  const [filterValue, setFilterValue] = useState('');
+  const [listToRender, setListToRender] = useState(list);
 
-  const getValue = () => {
-    const value = list.find((item) => item.content === today.emotion)?.value;
-    return value ? [value] : [];
+  useEffect(() => {
+    const filteredList = list.map((group) => {
+      return {
+        title: group.title,
+        items: group.items.filter((item) => {
+          return item.toLowerCase().includes(filterValue.toLowerCase());
+        }),
+      };
+    });
+    setListToRender(filteredList);
+  }, [filterValue]);
+
+  const onChange = (value: string) => {
+    onAnswer(!!value);
+    updateToday({ emotion: value });
   };
-  const onUpdate = (value: string[]) => {
-    onAnswer(!!value[0]);
-    updateToday({ emotion: list.find((item) => item.value === value[0])?.content });
+
+  const onChangeFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(e.target.value);
   };
+
   return (
     <div className="emotion-select">
-      <div className="emotion-select__field"></div>
-      <div className="emotion-select__list"></div>
-      <Select
+      <div className="emotion-select__menu">
+        <div className="emotion-select__filter">
+          <input type="text" value={filterValue} onChange={onChangeFilter} placeholder="Фильтр" />
+        </div>
+        <div className="emotion-select__list">
+          {listToRender.map((group) => {
+            return (
+              <div className="emotion-select__group" key={group.title}>
+                {/* <div className="emotion-select__title">{group.title}</div> */}
+                <div className="emotion-select__options">
+                  {group.items.map((option) => {
+                    return (
+                      <div
+                        className={classnames('emotion-select__option', {
+                          'emotion-select__option_active': option === today.emotion,
+                        })}
+                        key={option}
+                      >
+                        <input id={option} name="emotion" type="radio" onChange={() => onChange(option)} />
+                        <label
+                          htmlFor={option}
+                          style={{ borderColor: option === today.emotion ? today.color : 'transparent' }}
+                        >
+                          {option}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <div className="emotion-select__field">{today.emotion}</div> */}
+      {/* <Select
         id="emotion"
+        // popupWidth={900}
+        // popupPlacement={['top-start']}
         value={getValue()}
         options={list}
         filterable
         placeholder="Выберите эмоцию"
         onUpdate={onUpdate}
-      />
+      /> */}
     </div>
   );
 };
