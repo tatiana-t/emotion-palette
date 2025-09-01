b36fdf8194b5ed2a305f3e7c419d0f6a07e52fcd
Tatiana <grabli_77@mail.ru>
2025-09-01 16:55:38 +0300

    test pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from '@gravity-ui/uikit';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.tsx';
 // import '@gravity-ui/uikit/styles/fonts.css';
 // import '@gravity-ui/uikit/styles/styles.css';
 import './styles/ui-theme.css';
 import './index.scss';
+
+const updateSW = registerSW({
+  immediate: true,
+  // onNeedRefresh() {
+  //   // show a prompt to user
+  //   const answer = prompt('Need refresh');
+  //   console.log('answer', answer);
+  //   if (answer) {
+  //     updateSW();
+  //   }
+  // },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      // registerType: 'autoUpdate',
+      includeAssets: ['/public/logo.svg'],
+      strategies: 'injectManifest',
+      // srcDir: 'src/workers',
+      // filename: 'sw.ts',
+      // devOptions: {
+      //   enabled: true, // Включить в dev (с ограничениями)
+      //   type: 'module', // Использовать модульный worker
+      // },
+      manifest: {
+        name: 'Палитра настроений',
+        short_name: 'Палитра',
+        start_url: '/palette',
+        display: 'standalone',
+        icons: [{ purpose: 'any', sizes: 'any', src: 'assets/icons/icon-palette.svg', type: 'image/svg' }],
+
+        theme_color: '#8936FF',
+        background_color: '#2EC6FE',
+        orientation: 'portrait',
+        lang: 'ru-RU',
+        scope: 'https://hereandnowcreation.art/palette',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       src: '/src',
