f7dd4bf729f619ca4edaa63df6d4dfd70a2cd193
Tatiana <grabli_77@mail.ru>
2025-09-04 19:05:12 +0300

    update history design

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import classnames from 'classnames';
 import { useDataStore, clearHistory } from 'src/storage';
 import type { IColor } from 'src/types';
@@ -110,6 +110,8 @@ const HistoryPage = () => {
     if (!currentItem) {
       return null;
     }
+    const optionalDescription: string | undefined = currentItem.compensate || currentItem.like;
+    console.log('optionalDescription', optionalDescription);
     return (
       // <div className="">
       <div
@@ -121,14 +123,18 @@ const HistoryPage = () => {
         {currentItem.reflection.map(({ id, value }) => {
           const questionText: string = questions.find((item) => item.id === id)?.text || '';
           return (
-            <Fragment key={id}>
-              <div className="page-history__description-item" key={id}>
-                <div className="page-history__description-title">{questionText}</div>
-                <div className="page-history__description-text">{value}</div>
-              </div>
-            </Fragment>
+            <div className="page-history__description-item" key={id}>
+              <div className="page-history__description-title">{questionText}</div>
+              <div className="page-history__description-text">{value}</div>
+            </div>
           );
         })}
+        {optionalDescription && (
+          <div className="page-history__description-item">
+            {/* <div className="page-history__description-title">{questionText}</div> */}
+            <div className="page-history__description-text">{optionalDescription}</div>
+          </div>
+        )}
       </div>
       // </div>
     );
@@ -136,8 +142,9 @@ const HistoryPage = () => {
 
   return (
     <div className="page-history">
+      <div className="page-history__title">Палитра настроений</div>
       <div className="page-history__list">
-        <button onClick={onClearHistory}>очистить</button>
+        {/* <button onClick={onClearHistory}>очистить</button> */}
         {listToRender.map((arr, i) => {
           return (
             <div className="page-history__row-wrapper" key={i}>
@@ -147,15 +154,28 @@ const HistoryPage = () => {
                     <div
                       key={item.colorId}
                       className={classnames('page-history__item', {
-                        'page-history__item_active': currentItem?.colorId === item.colorId,
+                        // 'page-history__item_active': currentItem?.colorId === item.colorId,
                       })}
                       onClick={() => updateCurrentItem(item)}
                     >
                       <div className="page-history__color" style={{ backgroundColor: item.color }} />
                       <div className="page-history__content">
-                        <div className="page-history__title">{item.date}</div>
-                        <div className="page-history__text">{item.targetEmotion}</div>
+                        <div className="page-history__item-title">{item.targetEmotion}</div>
+                        <div className="page-history__text">{item.date}</div>
                       </div>
+                      <svg
+                        className="page-history__open-button"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M6.85925 3.93096C6.41463 3.23227 6.62083 2.3046 7.31921 1.85967C8.01806 1.41495 8.94569 1.62088 9.3905 2.31963L13.7655 9.19463C14.0781 9.68597 14.0782 10.3146 13.7655 10.806L9.3905 17.681C8.94557 18.3793 8.0179 18.5855 7.31921 18.1409C6.62069 17.6962 6.41494 16.7684 6.85925 16.0696L10.7216 10.0003L6.85925 3.93096Z"
+                          fill="#8F8E8E"
+                        />
+                      </svg>
                     </div>
                   );
                 })}
