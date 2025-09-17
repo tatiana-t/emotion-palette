416c6f6ac93a4abdaff139637e17ab7304242f8f
Tatiana <grabli_77@mail.ru>
2025-09-17 11:04:00 +0300

    edit tracker for prod only

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import LogRocket from 'logrocket';
 import { ThemeProvider } from '@gravity-ui/uikit';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App.tsx';
 // import '@gravity-ui/uikit/styles/fonts.css';
 // import '@gravity-ui/uikit/styles/styles.css';
-import LogRocket from 'logrocket';
-LogRocket.init('0493q8/emotion-palette');
 
 import './styles/ui-theme.css';
 import './index.scss';
+
+if (import.meta.env.PROD) {
+  LogRocket.init('0493q8/emotion-palette');
+}
 
 registerSW({
   immediate: true,
